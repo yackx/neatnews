d229f0918d70cb62f77056a6fe570c93dab72a1a
Youri Ackx <1193689+yackx@users.noreply.github.com>
2022-06-29 19:36:04 +0200

    Remove g4g experiment

--- poc.py
+++ poc.py
@@ -45,30 +45,7 @@ def crawl():
     print(f"{len(headlines)} articles")
 
 
-def g4g():
-    # Import the beautifulsoup
-    # and request libraries of python.
-    import requests
-    import bs4
-
-    # Make two strings with default google search URL
-    # 'https://google.com/search?q=' and
-    # our customized search keyword.
-    # Concatenate them
-    text = "geeksforgeeks"
-    url = 'https://google.com/search?q=' + text
-
-    # Fetch the URL data using requests.get(url),
-    # store it in a variable, request_result.
-    request_result = requests.get(url)
-
-    # Creating soup from the fetched request
-    soup = bs4.BeautifulSoup(request_result.text,
-                             "html.parser")
-    print(soup)
-
-
 if __name__ == '__main__':
     # print_page("https://www.lecho.be/dossiers/conflit-ukraine-russie/cyberguerre-la-belgique-en-premiere-ligne-face-a-la-menace-russe/10371441.html")
     # print_google("https://www.lecho.be/dossiers/conflit-ukraine-russie/cyberguerre-la-belgique-en-premiere-ligne-face-a-la-menace-russe/10371441.html")
-    g4g()
+    crawl()
