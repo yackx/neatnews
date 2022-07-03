4ae2ea1ca27a147bdc8b96426f84bac119a12558
Edwin Hermans <edwin@herdani.be>
2022-07-03 12:04:52 +0200

    Allow result in raw (JSON) format

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import FileResponse, RedirectResponse
+from starlette.responses import FileResponse, RedirectResponse, JSONResponse
 
 from crawlers import crawler_by_code, newspapers_by_code
 from models import Headline
@@ -22,9 +22,30 @@ def split_headlines_in_categories(headlines: [Headline]) -> [str, [Headline]]:
 
 
 @app.get("/{newspaper}/{path:path}", response_class=HTMLResponse)
-async def article(newspaper: str, path: str, request: Request,):
+async def article(newspaper: str, path: str, request: Request, raw: bool = False):
     crawler = crawler_by_code(newspaper)
     article = crawler.fetch_article(path)
+
+    if raw:
+        return JSONResponse({
+            'article': {
+                'title': article.title,
+                'summary': article.summary,
+                'img': article.img,
+                'url': article.url,
+                'paragraphs': article.paragraphs,
+                'published_on': article.published_on,
+                'updated_on': article.updated_on,
+                'see_also': article.see_also
+            },
+            "newspaper": {
+                "name": crawler.name(),
+                "url": crawler.base_url(),
+            },
+            "newspapers": newspapers_by_code(),
+            "selected_newspaper": newspaper,
+        })
+
     return templates.TemplateResponse("article.html", {
         "request": request,
         "article": article,
@@ -38,10 +59,37 @@ async def article(newspaper: str, path: str, request: Request,):
 
 
 @app.get("/{newspaper}", response_class=HTMLResponse, name="newspaper")
-async def headlines(newspaper: str, request: Request):
+async def headlines(newspaper: str, request: Request, raw: bool = False):
     crawler = crawler_by_code(newspaper)
     headlines = crawler.fetch_headlines()
     headlines_in_categories = split_headlines_in_categories(headlines)
+
+    if raw:
+        return JSONResponse({
+            'headlines': [{
+                'title': h.title,
+                'category': h.category,
+                'url': h.url,
+                'internal_url': h.internal_url,
+                'paywall': h.paywall,
+            } for h in headlines],
+            'headlines_in_categories': [{
+                'category': category,
+                'headlines': [{
+                    'title': h.title,
+                    'url': h.url,
+                    'internal_url': h.internal_url,
+                    'paywall': h.paywall,
+                } for h in category_headlines],
+            } for (category, category_headlines) in headlines_in_categories],
+            "newspaper": {
+                "name": crawler.name(),
+                "url": crawler.base_url(),
+            },
+            "newspapers": newspapers_by_code(),
+            "selected_newspaper": newspaper,
+        })
+
     return templates.TemplateResponse("index.html", {
         "request": request,
         "headlines_in_categories": headlines_in_categories,
